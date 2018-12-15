3e45084681afda94e0d36f1a96e50fc6e2b81aa7
Infant Frontender <newbornfrontender@gmail.com>
2018-12-15 21:48:49 +0300

    Добавлена конфигурация для создания 2-х разрешений

--- sharp.js
+++ sharp.js
@@ -1,11 +1,28 @@
-const glob = require('glob');
 const sharp = require('sharp');
 
-glob('from/*.jpg', (err, files) =>
-  files.map((file) =>
-    sharp(file)
-      .jpeg({ quality: 40 })
-      .resize(320, 240)
-      .toFile(file.replace('from', 'to').replace('jpg', 'min.jpg')),
-  ),
-);
+// -----------------------------------------------------------------------------
+
+// const glob = require('glob');
+
+// glob('from/*.jpg', (err, files) =>
+//   files.map((file) =>
+//     sharp(file)
+//       .jpeg({ quality: 40 })
+//       .resize(320, 240)
+//       .toFile(file.replace('from', 'to').replace('jpg', 'min.jpg')),
+//   ),
+// );
+
+// -----------------------------------------------------------------------------
+
+const name = ['narrow', 'wide']
+const width = [320, 1280]
+const height = [240, 720]
+
+for (let i = 0; i < 2; i += 1) {
+  sharp('from/sample.png')
+    .resize(width[i], height[i])
+    .toFile(`to/sample-${name[i]}.png`)
+}
+
+// -----------------------------------------------------------------------------
